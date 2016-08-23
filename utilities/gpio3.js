66e2d5b693cc63a9c686840d675387661f806980
Dustin Smith <thedustinsmith@gmail.com>
2016-08-22 19:23:44 -0500

    updates

--- utilities/gpio3.js
+++ utilities/gpio3.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 // some libraries use pins, some use ports
 var PIN_NUMBERS = [7, 11, 12, 13, 15, 16, 18, 22];
 var PORT_NUMBERS = [4, 17, 18, 27, 22, 23, 24, 25];
-var inits = [];
+var gpios = {}
 var gpio;
 try {
     gpio = require('gpio');
@@ -18,26 +18,29 @@ catch(e) {
     };
 }
 
-function set (port, val, cb) {
-    var g = gpio.export(PORT_NUMBERS[port], {
-        ready: function() { 
-            g.set(val);
-            cb();
+for (var i = 0; i< PORT_NUMBERS.lenth; i++) {
+    var p = PORT_NUMBERS[i];
+    gpios[p.toString()] = gpio.export(p, {
+        ready: function() {
+            this.set(1);
         }
     });
 }
-function get( port, cb) {
-    port = PORT_NUMBERS[port];
-    var g = gpio.export(port, {
-        ready: function() {
-            g.get(cb);
-        }
-    });
+
+process.on('exit', function() {
+    for (var p in gpios) {
+        gpios[p].unexport();
+    }
+});
+
+function set (port, val, cb) {
+    gpios[port].set(val, cb);
+}
+function get(port) {
+    cb(gpios[port].value);
 }
 function toggle(port, cb) {
-    get(port, function(v) {
-        set(port, 1-vcb)
-    });
+    set(port, 1 - get(port), cb);
 }
 
 module.exports = {
